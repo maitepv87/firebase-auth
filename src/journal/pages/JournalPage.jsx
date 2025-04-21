f05b39423abde84fb57e47140b4684505d53ce55
Maite Perez Vento <maitepv87@gmail.com>
2025-04-21 12:56:29 -0400

    feat: improve JournalPage layout and greet user by displayName

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { Button } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { Button, Container, Paper, Typography, Box } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { startLogout } from "../../store/auth";
 
 export const JournalPage = () => {
+  const { displayName } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const handleLogout = () => {
@@ -12,18 +13,27 @@ export const JournalPage = () => {
   };
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Journal Page</h1>
+    <Container maxWidth="sm" sx={{ mt: 8 }}>
+      <Paper elevation={3} sx={{ p: 4, textAlign: "center", borderRadius: 4 }}>
+        <Typography variant="h4" gutterBottom>
+          Welcome, {displayName}!
+        </Typography>
 
-      <Button
-        variant="outlined"
-        color="error"
-        startIcon={<LogoutIcon />}
-        onClick={handleLogout}
-        sx={{ mt: 2 }}
-      >
-        Logout
-      </Button>
-    </div>
+        <Typography variant="subtitle1" sx={{ mb: 4 }}>
+          This is your Journal. Start writing your thoughts and ideas.
+        </Typography>
+
+        <Box>
+          <Button
+            variant="contained"
+            color="error"
+            startIcon={<LogoutIcon />}
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
--- src/journal/routes/JournalRoutes.jsx
+++ src/journal/routes/JournalRoutes.jsx
@@ -3,12 +3,6 @@ import { JournalPage } from "../pages";
 
 export const JournalRoutes = () => {
   return (
-    // <Routes>
-    //   <Route path="/" element={<JournalPage />} />
-
-    //   <Route path="/*" element={<Navigate to="/" />} />
-    // </Routes>
-
     <Routes>
       <Route path="/" element={<JournalPage />} />
       {/* Cualquier otra ruta dentro de journal se redirige a la principal */}
